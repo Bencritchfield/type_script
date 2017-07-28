00a494245c47ff6ee464bfb04254221117f09492
Ben Critchfield <ben.critchfield8@gmail.com>
2017-07-28 11:53:45 -0600

    type script practice

---
+++ function_declarations.js
@@ -0,0 +1,14 @@
+console.log(fullName('Ben', 'Critchfield'));
+// console.log(otherFullName('Ben', 'Critchfield'));
+// console.log(thirdFullName('Ben', 'Critchfield'));
+function fullName(first, last) {
+    return first + " " + last;
+}
+var otherFullName;
+otherFullName = function (first, last) {
+    return first + " " + last;
+};
+var thirdFullName = function (first, last) {
+    return first + " " + last;
+};
+//# sourceMappingURL=function_declarations.js.map
